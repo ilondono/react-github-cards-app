a3854c6d856646b0426b50911f6b101d50329f1c
SURAMERICANA\ivanloqu <idlondonoq@sura.com.co>
2020-06-12 16:11:50 -0500

    - Refactor del commit anterior para manejar el estado de una forma más simple. - Adición de función que procesa el envío del formulario. - Adición de método onChange para que React pueda controlar la escritura en el textfield (que en el commit pasado no se podía).

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 
 class Form extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {username: ''};
+    state = {
+        username: ''
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.username);
     }
 
     render() {
         return(
             <div style={{textAlign:'center'}}>
-                <form action=''>
+                <form onSubmit={this.handleSubmit}>
                     <input type="text" 
                            value={this.state.username}
                            placeholder="Type a GitHub username"
+                           onChange={(event) => this.setState({username: event.target.value})}
+                           required
                     />
                     <button>Add</button>
                 </form>
