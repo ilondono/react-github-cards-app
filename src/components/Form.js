95451ad9742aa4e866dc494ed8d34fee77dcc1ee
SURAMERICANA\ivanloqu <idlondonoq@sura.com.co>
2020-06-12 15:55:12 -0500

    - Se adiciona el estado para enlazar con el valor del campo de texto que ingresa el usuario. (El valor no podrá ser leído por React, así que no se puede digitar en el textfield)

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 
 class Form extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {username: ''};
+    }
+
     render() {
         return(
             <div style={{textAlign:'center'}}>
                 <form action=''>
                     <input type="text" 
+                           value={this.state.username}
                            placeholder="Type a GitHub username"
                     />
                     <button>Add</button>
